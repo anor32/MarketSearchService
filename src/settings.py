32c546f53e32ab1dc7472e9712021643a3de0e02
anor23 <burgchiken@gmail.com>
2026-06-29 16:58:01 +0300

    add fiedl validator to Postgress connection string

--- src/settings.py
+++ src/settings.py
@@ -1,3 +1,6 @@
+import re
+
+from pydantic import field_validator
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
@@ -11,3 +14,11 @@ class Settings(BaseSettings):
     KAFKA_TOPIC_ADS: str = "ads"
     kafka_consumer_group: str = "search-service"
     ad_service_url: str = "http://localhost:8002"
+
+    @field_validator("POSTGRES_CONNECTION_STRING", mode="before")
+    @classmethod
+    def ensure_asyncpg_driver(cls, v: str) -> str:
+        if isinstance(v, str):
+            if v.startswith("postgres://") or v.startswith("postgresql://"):
+                v = re.sub(r"^postgres(?:ql)?://", "postgresql+asyncpg://", v)
+        return v
